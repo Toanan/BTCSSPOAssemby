ccdc59afef7ad8f76620276cc89edb7a0c7057e3
Antoine Galland <toanan31@gmail.com>
2019-03-08 17:25:28 +0100

    Auth + Credentials

---
+++ BTCSSPOAssemby/AuthenticationFactory.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.SharePoint.Client;
+using System.Net;
+using OfficeDevPnP.Core;
+
+namespace BTCSSPOAssemby
+{
+    public static class AuthenticationFactory
+    {
+        /// <summary>
+        /// Retrieve an authenticated ClientContext using ICredentials
+        /// </summary>
+        /// <param name="Url">The SharePoint Online site Url</param>
+        /// <param name="credentials">The credentials associated</param>
+        /// <returns>A tested authenticated ClientContext</returns>
+        public static ClientContext GetAuthenticatedContext(string Url, ICredentials credentials)
+        {
+            var context = new ClientContext(Url);
+            context.Credentials = credentials;
+
+            // We try to call for the SPO site title
+            validateAuthentication(context);
+            return context;
+        }
+
+        /// <summary>
+        /// REtrieve an authenticated ClientContext using AppOnly auth
+        /// </summary>
+        /// <param name="Url">The SharePoint Online site Url</param>
+        /// <param name="AppID">The App id</param>
+        /// <param name="AppSecret">The App secret</param>
+        /// <returns>A tested authenticated ClientContext</returns>
+        public static ClientContext GetAuthenticatedContext(string Url, string AppID, string AppSecret)
+        {
+            var am = new OfficeDevPnP.Core.AuthenticationManager();
+            var context = am.GetAppOnlyAuthenticatedContext(Url, AppID, AppSecret);
+
+            // We try to call for the SPO site title
+            validateAuthentication(context);
+            return context;
+        }
+
+        /// <summary>
+        /// Validate the Authentication by calling for the web.title
+        /// </summary>
+        /// <param name="context">The clientcontext to validate</param>
+        private static void validateAuthentication(ClientContext context)
+        {
+            using (context)
+            {
+                context.Load(context.Web, w => w.Title);
+                context.ExecuteQuery();
+            }
+        }
+    }
+}
---
+++ BTCSSPOAssemby/CredentialFactory.cs
@@ -0,0 +1,121 @@
+﻿using System.Net;
+using System.Security;
+using Microsoft.SharePoint.Client;
+using CredentialManagement;
+using OfficeDevPnP.Core.Utilities;
+
+namespace BTCSSPOAssemby
+{
+    public static class CredentialFactory
+    {
+        /// <summary>
+        /// Describe the authentication type to create the credentials accordingly
+        /// </summary>
+        public enum CredentialsType
+        {
+            /// <summary>
+            /// SharePointOnlineCredentials
+            /// </summary>
+            SharePointOnline,
+            /// <summary>
+            /// NetworkCredentials
+            /// </summary>
+            SharePointActiveDirectory
+        }
+
+        /// <summary>
+        /// Create a credentials object according to the authentication type
+        /// </summary>
+        /// <param name="userName">UserName or AppID</param>
+        /// <param name="passWord">Password or AppSecret</param>
+        /// <param name="authenticationType">The authentication type</param>
+        /// <returns>ICredential implementation object</returns>
+        public static ICredentials CreateCredentials(string userName, string passWord, CredentialsType authenticationType)
+        {
+            ICredentials result = null;
+
+            switch (authenticationType)
+            {
+                case CredentialsType.SharePointOnline:
+                    SecureString securePassword = new SecureString();
+                    foreach (char c in passWord.ToCharArray())
+                    {
+                        securePassword.AppendChar(c);
+                    }
+                    result = new SharePointOnlineCredentials(userName, securePassword);
+                    break;
+                case CredentialsType.SharePointActiveDirectory:
+                    result = new NetworkCredential(userName, passWord);
+                    break;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Create a credentials object according to the authentication type
+        /// </summary>
+        /// <param name="userName">UserName</param>
+        /// <param name="securePassword">Password</param>
+        /// <param name="authenticationType">The authentication Type</param>
+        /// <returns>ICredential implementation object</returns>
+        public static ICredentials CreateCredentials(string userName, SecureString securePassword, CredentialsType authenticationType)
+        {
+            ICredentials result = null;
+
+            switch (authenticationType)
+            {
+                case CredentialsType.SharePointOnline:
+                    result = new SharePointOnlineCredentials(userName, securePassword);
+                    break;
+                case CredentialsType.SharePointActiveDirectory:
+                    result = new NetworkCredential(userName, securePassword);
+                    break;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Save credentials in the windows credential manager
+        /// </summary>
+        /// <param name="Name">Credential label (name)</param>
+        /// <param name="UserName">The UserName</param>
+        /// <param name="SecurePassWord">The secure password</param>
+        /// <returns>A SharepointOnlineCredentials object</returns>
+        public static SharePointOnlineCredentials StoreCredentials(string Name, string UserName, SecureString SecurePassWord)
+        {
+            Credential Cred = new Credential
+            {
+                Target = Name,
+                Username = UserName,
+                SecurePassword = SecurePassWord,
+                PersistanceType = PersistanceType.LocalComputer,
+                Type = CredentialType.Generic,
+            };
+            Cred.Save();
+            SharePointOnlineCredentials cred = CredentialManager.GetSharePointOnlineCredential(Name);
+            return cred;
+        } 
+
+        /// <summary>
+        /// Retrieve credentials from the windows credential manager
+        /// </summary>
+        /// <param name="Name">Credential label (name)</param>
+        /// <param name="authenticationType">The authentication Type</param>
+        /// <returns></returns>
+        public static ICredentials GetStoredCredentials(string Name, CredentialsType authenticationType)
+        {
+            ICredentials result = null;
+
+            switch (authenticationType)
+            {
+                case CredentialsType.SharePointOnline:
+                    result = CredentialManager.GetSharePointOnlineCredential(Name);
+                    break;
+                case CredentialsType.SharePointActiveDirectory:
+                    result = CredentialManager.GetCredential(Name);
+                    break;
+            }
+            return result;
+        }
+    }
+}
